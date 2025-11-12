4e85c4fcf7f787323d830d89e48a892fd98f965b
Takaesi <takaesi@yandex.ru>
2025-11-12 18:24:45 +0300

    added home page

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,9 +1,54 @@
-import './home.css'
+import "./home.css";
+import { Link } from "react-router-dom";
 export default function Home() {
-    return (
+  return (
     <div>
-        <h1>HomePage</h1>
+      <header>
+        <p className="name">ShopManager</p>
+        <nav className="headerFlex">
+          <Link to="/login" className="btn">
+            Login
+          </Link>
+          <Link to="/register" className="btn">
+            Register
+          </Link>
+        </nav>
+      </header>
+      <main>
+        <h1 className="mainH1">About functional:</h1>
+        <div className="mainFlex">
+            <div className="card">
+                <h2>Клиенты</h2>
+                <ul>
+                    <li>Список клиентов</li>
+                    <li>Поиск клиента</li>
+                    <li>Добавить клиента</li>
+                    <li>Редактировать клиента</li>
+                    <li>Удалить клиента</li>
+                </ul>
+            </div>
+            <div className="card">
+                <h2>Продукты</h2>
+                <ul>
+                    <li>Список продуктов</li>
+                    <li>Поиск продукта</li>
+                    <li>Добавить продукт</li>
+                    <li>Редактировать продукт</li>
+                    <li>Удалить продукт</li>
+                </ul>
+            </div>
+            <div className="card">
+                <h2>Заказы</h2>
+                <ul>
+                    <li>Список заказов</li>
+                    <li>Поиск заказа</li>
+                    <li>Добавить заказ</li>
+                    <li>Редактировать заказ</li>
+                    <li>Удалить заказ</li>
+                </ul>
+            </div>
+        </div>
+      </main>
     </div>
-    )
+  );
 }
-
